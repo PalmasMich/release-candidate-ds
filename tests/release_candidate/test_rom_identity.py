656bdbb4a573a068f5fb99c05fe63193d04547b7
Michael Palmas <mky180@Gmail.com>
2026-09-12 06:49:23 -0700

    test: define Release Candidate DS banner identity

--- tests/release_candidate/test_rom_identity.py
+++ tests/release_candidate/test_rom_identity.py
@@ -9,6 +9,12 @@ class RomIdentityTests(unittest.TestCase):
         parser.read(Path("platinum.us/rom_rev1.ini"), encoding="utf-8")
         self.assertEqual(parser["header"]["title"], "RELEASE CAND")
 
+    def test_rev1_banner_identifies_release_candidate(self):
+        parser = configparser.ConfigParser()
+        parser.read(Path("platinum.us/rom_rev1.ini"), encoding="utf-8")
+        self.assertEqual(parser["banner"]["title"], "Release Candidate")
+        self.assertEqual(parser["banner"]["developer"], "PalmasMich")
+
 
 if __name__ == "__main__":
     unittest.main()
